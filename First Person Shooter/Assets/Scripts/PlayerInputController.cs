22fe3c341c6d7c3671c1f3b50556396c08e30ef7
Ionel <ionelalexandru.pop@gmail.com>
2021-01-27 19:04:18 -0300

    Simple Character Controller Working

---
+++ First Person Shooter/Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public PlayerInput Current;
+
+    // Agregar cool downs here
+    public float jumpPressedRememberTime = 0.25f;
+    public float jumpPressedRemember;
+
+    private void Start()
+    {
+        Current = new PlayerInput();
+    }
+
+    private void Update()
+    {
+        Vector3 moveInputRaw = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        jumpPressedRemember -= jumpPressedRemember > 0 ? Time.deltaTime : 0;
+
+        // Jump Input
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressedRemember = jumpPressedRememberTime;
+        }
+
+        bool interactInput = Input.GetKeyDown(KeyCode.F);
+        bool lightAttackInput = Input.GetKeyDown(KeyCode.Mouse0);
+        bool strongAttackInput = Input.GetKeyDown(KeyCode.Mouse1);
+        bool dashInput = Input.GetKeyDown(KeyCode.LeftShift);
+
+        Current = new PlayerInput()
+        {
+            MoveInputRaw = moveInputRaw,
+            MoveInput = moveInput,
+            MouseInput = mouseInput,
+            JumpInput = jumpPressedRemember > 0f,
+            LightAttackInput = lightAttackInput,
+            StrongAttackInput = strongAttackInput,
+            InteractInput = interactInput,
+            DashInput = dashInput,
+        };
+    }
+    public struct PlayerInput
+    {
+        public Vector3 MoveInputRaw;
+        public Vector3 MoveInput;
+        public Vector2 MouseInput;
+        public bool JumpInput;
+        public bool LightAttackInput;
+        public bool StrongAttackInput;
+        public bool InteractInput;
+        public bool DashInput;
+    }
+}
---
+++ First Person Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum SimpleStateMachine
+{
+    Idle,
+    Moving,
+    Jumping,
+    Falling,
+    Dashing,
+    Shooting
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("State Machine")]
+    public SimpleStateMachine currentState;
+
+    [Header("Player Settings")]
+    public Transform playerMainCamera;
+
+    private CharacterController characterController;
+    private PlayerInputController inputController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        inputController = GetComponent<PlayerInputController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Aca funciona
+        HandleInputs();
+        RotateCamera();
+
+        switch (currentState)
+        {
+            case SimpleStateMachine.Idle:
+                moveDirection = GetMoveDirection(movementInput);
+
+                if (moveDirection.x != 0f && moveDirection.z != 0f)
+                    currentState = SimpleStateMachine.Moving;
+
+                SetGravity();
+
+                if (jumpInput && isGrounded)
+                {
+                    UseJump();
+                    currentState = SimpleStateMachine.Jumping;
+                }
+
+                characterController.Move(moveDirection * Time.deltaTime);
+                break;
+            case SimpleStateMachine.Moving:
+                moveDirection = GetMoveDirection(movementInput);
+
+                if (moveDirection.x == 0f && moveDirection.z == 0f)
+                    currentState = SimpleStateMachine.Idle;
+
+                SetGravity();
+                
+                if (jumpInput && isGrounded)
+                {
+                    UseJump();
+                    currentState = SimpleStateMachine.Jumping;
+                }
+
+                characterController.Move(moveDirection * Time.deltaTime);
+                break;
+            case SimpleStateMachine.Jumping:
+                moveDirection = GetMoveDirection(movementInput);
+                SetGravity();
+                characterController.Move(moveDirection * Time.deltaTime);
+
+                if (isGrounded)
+                {
+                    currentState = SimpleStateMachine.Moving;
+                }
+                break;
+        }
+    }
+
+    #region Inputs
+
+    [Header("Debug Inputs")]
+    public Vector3 movementInput;
+    public Vector3 mouseInput;
+    public bool isGrounded;
+    public bool jumpInput;
+    public bool dashInput;
+
+    private void HandleInputs()
+    {
+        movementInput = inputController.Current.MoveInputRaw.normalized;
+        mouseInput = inputController.Current.MouseInput;
+
+        jumpInput = inputController.Current.JumpInput;
+
+        isGrounded = GetGroundedStatus();
+    }
+
+    #endregion
+
+    #region Camera Rotation and Look
+
+    [Header("Camera Rotation and Look")]
+    public float sensitivity = 100f;
+    public float xRotation = 0f;
+
+    private void RotateCamera()
+    {
+        float mouseX = mouseInput.x * sensitivity * Time.deltaTime;
+        float mouseY = mouseInput.y * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerMainCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    #endregion
+
+    #region Movement and Rotation
+
+    [Header("Player Movement and Rotation")]
+    public float movementSpeed = 10f;
+
+    [Header("Movement Debug")]
+    public Vector3 moveDirection;
+    public Vector3 camForward;
+    public Vector3 camRight;
+
+    private void GetCamDirection()
+    {
+        camForward = playerMainCamera.forward;
+        camRight = playerMainCamera.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+    }
+
+    private Vector3 GetMoveDirection(Vector3 input)
+    {
+        GetCamDirection();
+
+        return (input.x * camRight + input.z * camForward) * movementSpeed;
+    }
+
+    #endregion
+
+    #region Abilities
+
+    [Header("Abilities")]
+    public float jumpForce = 15f;
+
+    private void UseJump()
+    {
+        Debug.Log("Jump");
+        fallVelocity = jumpForce;
+        moveDirection.y = fallVelocity;
+    }
+
+    #endregion
+
+    #region Gravity and Ground Detection
+
+    [Header("Gravity Settings")]
+    public float gravity = 40f;
+    public float slideVelocity = 7f;
+    public float slopeForceDown = 10f;
+
+    [Header("Ground Time Remember")]
+    public float groundedRememberTime = 0.1f;
+
+    [Header("Debug Gravity and Ground Detection")]
+    public Vector3 hitNormal;
+    public float fallVelocity;
+    public bool isOnSlope;
+    public float groundedRemember;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hitNormal = hit.normal;
+    }
+
+    private void SetGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            moveDirection.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            moveDirection.y = fallVelocity;
+        }
+
+        SlideDown();
+    }
+
+    private void SlideDown()
+    {
+        isOnSlope = Vector3.Angle(Vector3.up, hitNormal) > characterController.slopeLimit;
+
+        if (isOnSlope)
+        {
+            moveDirection.x += ((1f - hitNormal.y) * hitNormal.x) * slideVelocity;
+            moveDirection.z += ((1f - hitNormal.y) * hitNormal.z) * slideVelocity;
+            moveDirection.y -= slopeForceDown;
+        }
+    }
+
+    private bool GetGroundedStatus()
+    {
+        groundedRemember -= groundedRemember > 0 ? Time.deltaTime : 0;
+
+        if (characterController.isGrounded)
+        {
+            groundedRemember = groundedRememberTime;
+        }
+
+        return groundedRemember > 0f;
+    }
+
+    #endregion
+}
